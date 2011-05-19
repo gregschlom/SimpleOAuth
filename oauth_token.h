1c2bf78434d309016932f813f0bf7f3c46e653c2
Gregory Schlomoff <gregory.schlomoff@gmail.com>
2011-05-19 11:03:48 +0200

    Cleaning whitespace

--- oauth_token.h
+++ oauth_token.h
@@ -1,19 +1,19 @@
 /*
  *  SimpleOauth - A simple OAuth authentication library for Qt
- *  
+ *
  *  Copyright (C) 2010 Gregory Schlomoff <gregory.schlomoff@gmail.com>
  *                     http://gregschlom.com
- *  	
+ *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is
  *  furnished to do so, subject to the following conditions:
- *  
+ *
  *  The above copyright notice and this permission notice shall be included in
  *  all copies or substantial portions of the Software.
- *  
+ *
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -40,10 +40,10 @@ class Token
 public:
 	enum TokenType {
 		InvalidToken,
-		RequestToken,	
+		RequestToken,
 		AccessToken
 	};
-	
+
 	enum AuthMethod {
 		HttpHeader,
 		Sasl
@@ -53,7 +53,7 @@ public:
 	Token(const Token& other);
 	Token &operator=(const Token&);
 	~Token();
-	
+
 	void setType(Token::TokenType type);
 	void setConsumerKey(const QString& consumerKey);
 	void setConsumerSecret(const QString& consumerSecretKey);
@@ -65,13 +65,13 @@ public:
 	Token::TokenType type() const;
 	QString tokenString() const;
 	QString tokenSecret() const;
-	
+
 	QByteArray signRequest(const QUrl& requestUrl, Token::AuthMethod authMethod = HttpHeader);
 
 private:
 	QString generateSignature(const QUrl& requestUrl, const QMap<QString, QString>& requestParameters);
 	QString hmac_sha1(const QString &message, const QString &key);
-	
+
     friend class TokenPrivate;
 	QSharedDataPointer<TokenPrivate> d;
 };
